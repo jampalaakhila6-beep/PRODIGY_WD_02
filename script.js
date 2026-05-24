390e101c69b33d73559ef0c22c246c6456ab92d6
jampalaakhila6-beep <jampalaakhila6@gmail.com>
2026-05-24 22:22:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let timer = null;
+
+function updateDisplay(){
+    let h =
+    hours < 10 ? "0"+hours : hours;
+
+    let m =
+    minutes < 10 ? "0"+minutes : minutes;
+
+    let s =
+    seconds < 10 ? "0"+seconds : seconds;
+
+document.getElementById("display")
+.innerHTML =
+`${h} : ${m} : ${s}`;
+}
+
+
+function stopwatch(){
+
+    seconds++;
+
+    if(seconds==60){
+        seconds=0;
+        minutes++;
+    }
+
+    if(minutes==60){
+        minutes=0;
+        hours++;
+    }
+
+updateDisplay();
+
+}
+
+function start(){
+
+if(timer!==null){
+clearInterval(timer);
+}
+
+timer =
+setInterval(stopwatch,1000);
+
+}
+
+
+function stop(){
+clearInterval(timer);
+}
+
+
+function reset(){
+
+clearInterval(timer);
+
+seconds=0;
+minutes=0;
+hours=0;
+
+updateDisplay();
+
+document.getElementById("laps")
+.innerHTML="";
+}
+
+
+function lap(){
+
+let li =
+document.createElement("li");
+
+li.innerText =
+document.getElementById("display")
+.innerText;
+
+document.getElementById("laps")
+.appendChild(li);
+
+}
